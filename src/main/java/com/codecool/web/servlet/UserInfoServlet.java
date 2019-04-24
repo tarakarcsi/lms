f4a1c518793fc20121dfa8202011d37c5d282990
beszteri <lajka89@gmail.com>
2019-04-24 13:12:22 +0200

    user info servlet

---
+++ src/main/java/com/codecool/web/servlet/UserInfoServlet.java
@@ -0,0 +1,20 @@
+package com.codecool.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/UserInfo")
+public class UserInfoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.sendRedirect("UserInfo.html");
+
+
+    }
+}
