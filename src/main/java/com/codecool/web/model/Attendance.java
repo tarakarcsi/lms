259ee85b18d7a7999ad7d32f0646427283604604
barnadenes <nyukk1007@gmail.com>
2019-05-08 14:44:38 +0200

    Attendance class done

--- src/main/java/com/codecool/web/model/Attendance.java
+++ src/main/java/com/codecool/web/model/Attendance.java
@@ -1,4 +1,32 @@
 package com.codecool.web.model;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Attendance {
+    private User user;
+    private Date date;
+    private String present;
+
+    public Attendance(User user, Date date, String present) {
+        this.user = user;
+        this.date = date;
+        this.present = present;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY - MM - dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    public String getPresent() {
+        return present;
+    }
 }
